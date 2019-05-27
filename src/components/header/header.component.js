321dca7298dec012b4ffae8caed994d83896bc7d
sajanv88 <sajankumar.v@outlook.com>
2019-05-27 08:56:16 +0200

    added react-router

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import {Navbar, Nav, Container} from 'react-bootstrap';
-
+import { Link } from 'react-router-dom';
 export default class HeaderComponent extends React.PureComponent {
     render() {
         return (
             <div className="header bg-black">
                 <Navbar bg="dark" expand="lg">
-                    <Navbar.Brand href="/" className="text-white">React-SSR Kit</Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/" className="text-white">React-SSR Kit</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" className="bg-white"/>
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                            <Nav.Link href="/" className="text-white">Home</Nav.Link>
-                            <Nav.Link href="/settings" className="text-white">Configure</Nav.Link>
+                            <Nav.Link as={Link} to="/" className="text-white">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/settings" className="text-white">Configure</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
